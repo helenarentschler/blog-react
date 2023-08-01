dec14514a1bb806064aab7afc8d3aaaf9c1ad6e2
Helena Rentschler <helrentschler@gmail.com>
2023-08-01 18:16:58 -0300

    load & error

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -4,22 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlog] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => {
+                if(!res.ok) {
+                    throw Error("could not fetch the data");
+                }
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlog(data);
+                setIsLoading(false);
+                setError(null);
             }) 
+            .catch(err => {
+                setError(err.message);
+                setIsLoading(false);
+            });
     }, []);
 
     return ( 
         <div className="home">
-            {/* conditional template */}
-            { blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+            { error && <div> { error } </div> }
+            { isLoading && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs"/> }
         </div>
      );
 }
