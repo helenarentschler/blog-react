ce96a855e408d0e770def84677bc695fec88c921
Helena Rentschler <helrentschler@gmail.com>
2023-08-01 16:05:58 -0300

    use effect

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,16 +8,24 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const [name, setName] = useState("mario");
    
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlog(newBlogs);
     }
 
+    // loads every time the state changes (re-render)
+    useEffect(() => {
+        console.log("use effect ran");
+        console.log(name);
+    }, [name]);
+    //is there's dependency, only if dependency changes 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/>
+            <button onClick={() => (setName("helena"))}>change name</button>
         </div>
      );
 }
