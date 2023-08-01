db93b63c3f4660f7bebbaa6426fcdbc9af70c45e
Helena Rentschler <helrentschler@gmail.com>
2023-08-01 14:56:09 -0300

    filter

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -1,10 +1,9 @@
 
-const BlogList = (props) => {
+const BlogList = ({blogs, title}) => {
 
-    const blogs = props.blogs;
-    
     return ( 
         <div className="blog-list">
+            <h1>{title}</h1>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
